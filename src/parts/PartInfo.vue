39ebb48a0c01d2dbafa8a5beb0ec3a52824a1d2c
Alejandro Molines <alemol32@gmail.com>
2019-10-02 22:28:55 +0200

    first commit

---
+++ src/parts/PartInfo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1> {{part.title}}</h1>
+    <div>
+      {{part.description}}
+    </div>
+     <img :src="part.src" title="arm"/>
+  </div>
+</template>
+
+<script>
+import parts from '../data/parts';
+
+export default {
+  name: 'PartInfo',
+  props: {
+    partType: {
+      type: String,
+    },
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
+      },
+    },
+  },
+  computed: {
+    part() {
+      const { partType, id } = this;
+      return parts[partType].find(part => part.id === +id);
+    },
+  },
+};
+</script>
+
+<style  scoped>
+img {
+  width:165px;
+}
+</style>
